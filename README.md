fdc5f4917cfec81ed7416ebc04068baba070e365
Corentin VÉROT <corentin.verot@outlook.com>
2021-04-19 20:39:10 +0200

    Amélioration CLI

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# steganography
+# Stéganographie [WIP]
+
+Ce projet vise à proposer un outil permettant de cacher des fichiers dans une image.
+Pour ce faire, une image au format bmp est nécessaire.
+
+## Principe
+Les octets du fichier à cacher seront décomposé et écraseront les LSB (bits de poids faible) 
+des pixels, engendrant une légère modification des couleurs de l'image originale 
+invisible à l'œil humain.
+
+## Utilisation du programme
+Un programme pour cacher des fichiers dans une image
+
+USAGE:
+steganography --input <input-file-path> --output <output-file-path> <SUBCOMMAND>
+
+FLAGS:
+-h, --help       Prints help information
+-V, --version    Prints version information
+
+OPTIONS:
+-i, --input <input-file-path> Fichier d'entrée
+-o, --output <output-file-path>    Fichier de sortie
+
+SUBCOMMANDS:
+decode    Mode décodage
+encode    Mode encodage
+help      Prints this message or the help of the given subcommand(s)
--- src/cli.rs
+++ src/cli.rs
@@ -7,52 +7,61 @@ use steganography::{ContainedFile, ContainerFile};
 
 
 #[derive(StructOpt, Debug)]
-enum SteganographyMode {
-    Decode,
-    Encode {
-        container_file_path: PathBuf
+#[structopt(
+    name = "steganography",
+    about = "Un programme pour cacher des fichiers dans une image",
+    author = "Corentin VÉROT",
+)]
+enum SteganographyCliArgs {
+    /// Mode décodage
+    Decode {
+        /// Fichier d'entrée
+        #[structopt(short = "-i", long = "--input", parse(from_os_str))]
+        input_file_path: PathBuf,
+
+        /// Fichier de sortie
+        #[structopt(short = "-o", long = "--output", parse(from_os_str))]
+        output_file_path: PathBuf,
     },
-}
 
+    /// Mode encodage
+    Encode {
+        /// Fichier bmp conteneur (dans lequel stocker les données à cacher)
+        #[structopt(short = "-c", long = "--container", parse(from_os_str))]
+        container_file_path: PathBuf,
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "steganography", about = "Un programme pour cacher des fichiers dans une image")]
-struct SteganographyCliArgs {
-    /// Mode (encodage ou décodage)
-    #[structopt(subcommand)]
-    mode: SteganographyMode,
+        /// Fichier d'entrée
+        #[structopt(short = "-i", long = "--input", parse(from_os_str))]
+        input_file_path: PathBuf,
 
-    /// Fichier d'entrée
-    #[structopt(short = "-i", long = "--input", parse(from_os_str))]
-    input_file_path: PathBuf,
-
-    /// Fichier de sortie
-    #[structopt(short = "-o", long = "--output", parse(from_os_str))]
-    output_file_path: PathBuf,
+        /// Fichier de sortie
+        #[structopt(short = "-o", long = "--output", parse(from_os_str))]
+        output_file_path: PathBuf,
+    },
 }
 
 pub fn run() -> Result<()> {
     let args = SteganographyCliArgs::from_args();
 
-    match args.mode {
-        SteganographyMode::Encode { container_file_path } => {
+    match args {
+        SteganographyCliArgs::Encode { container_file_path, input_file_path, output_file_path } => {
             let container_file = fs::File::open(container_file_path)?;
             let mut container_file = ContainerFile::from_file(container_file)?;
 
-            let source_file = fs::File::open(args.input_file_path)?;
+            let source_file = fs::File::open(input_file_path)?;
             let source_file = ContainedFile::from_file(source_file)?;
 
             container_file.encode(&source_file)?;
-            container_file.save(&args.output_file_path)?;
+            container_file.save(&output_file_path)?;
         }
-        SteganographyMode::Decode => {
-            let container_file = fs::File::open(args.input_file_path)?;
+        SteganographyCliArgs::Decode { input_file_path, output_file_path } => {
+            let container_file = fs::File::open(input_file_path)?;
             let container_file = ContainerFile::from_file(container_file)?;
 
             let mut contained_file = ContainedFile::from_container_file(&container_file);
 
             container_file.decode(&mut contained_file)?;
-            contained_file.save(&args.output_file_path)?;
+            contained_file.save(&output_file_path)?;
         }
     }
 
